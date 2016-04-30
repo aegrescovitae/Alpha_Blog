9f6aa7f81d6bdf0cbc1a8b7f8f15564a8994bad8
aegrescovitae <aegrescovitae@gmail.com>
2016-04-30 18:23:20 +0000

    Updated article.user == current_user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,7 @@ class ArticlesController < ApplicationController
    
    def create 
      @article = Article.new(article_params)
+     @article.user = current_user
      if @article.save
         flash[:success] = "Your article was posted successfully!"
         redirect_to article_path(@article)
